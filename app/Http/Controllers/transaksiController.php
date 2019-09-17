0caf8cfc44ef6fb3895af2ccb6ef873414bf4ee7
rifkialfaridzi <rifkinurlaila@gmail.com>
2019-09-17 08:56:39 +0700

    Fitur Transaksi

---
+++ app/Http/Controllers/transaksiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class transaksiController extends Controller
+{
+    public function index(){
+        return "halaman Transaksi";
+    }
+}
